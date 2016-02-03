092c11cdb09f6c3a41c841b2a8afe11623175a78
Peleg Rosenthal <peleg@teacherspayteachers.com>
2016-02-03 21:22:01 +0000

    Added Level 2/3

--- js/Game.js
+++ js/Game.js
@@ -136,27 +136,34 @@ TopDownGame.Game.prototype = {
     
     //player movement
     
+    var playerMaxSpeed = 50;
+    
+    // "cheat" code to go fast
+    if (this.input.keyboard.isDown(Phaser.KeyCode.SHIFT)) {
+      playerMaxSpeed = 1000;
+    }
+    
     this.player.body.velocity.x = 0;
     
     if(this.cursors.up.isDown) {
       this.playAnimation('up');
       if(this.player.body.velocity.y == 0) {
-        this.player.body.velocity.y -= 50;
+        this.player.body.velocity.y -= playerMaxSpeed;
       }
     } else if(this.cursors.down.isDown) {
       this.playAnimation('down');
       if(this.player.body.velocity.y == 0) {
-        this.player.body.velocity.y += 50;
+        this.player.body.velocity.y += playerMaxSpeed;
       }
     } else {
       this.player.body.velocity.y = 0;
     }
     if(this.cursors.left.isDown) {
       this.playAnimation('left');
-      this.player.body.velocity.x -= 50;
+      this.player.body.velocity.x -= playerMaxSpeed;
     } else if(this.cursors.right.isDown) {
       this.playAnimation('right');
-      this.player.body.velocity.x += 50;
+      this.player.body.velocity.x += playerMaxSpeed;
     }
   },
   collect: function(player, collectable) {
--- js/Preload.js
+++ js/Preload.js
@@ -14,6 +14,8 @@ TopDownGame.Preload.prototype = {
     //load game assets
     this.load.tilemap('level0', 'assets/tilemaps/level0.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.tilemap('level1', 'assets/tilemaps/level1.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.tilemap('level2', 'assets/tilemaps/level2.json', null, Phaser.Tilemap.TILED_JSON);
+    this.load.tilemap('level3', 'assets/tilemaps/level3.json', null, Phaser.Tilemap.TILED_JSON);
     this.load.image('gameTiles', 'assets/images/tiles.png');
     this.load.image('dungeontileset', 'assets/images/dungeontileset.png');
     this.load.image('greencup', 'assets/images/greencup.png');
